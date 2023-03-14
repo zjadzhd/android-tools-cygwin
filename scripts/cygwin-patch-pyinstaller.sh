84aded1be7fc92a29e30c2ce263543ad92a96af5
zjadzhd <116424564+zjadzhd@users.noreply.github.com>
2023-03-14 16:03:57 +0800

    Update cygwin-patch-pyinstaller.sh

--- scripts/cygwin-patch-pyinstaller.sh
+++ scripts/cygwin-patch-pyinstaller.sh
@@ -3,7 +3,7 @@
 python_ver=$(find /usr/local/lib -maxdepth 1 -type d -name python3.\* -printf '%P\n' | sort -V | head -n 1)
 
 cd /usr/local/lib/"${python_ver}"/site-packages || exit
-wget https://gist.github.com/ookiineko/9099b4c13bf56ddf0724fceea0c0cc91/raw/0c843c58632034e2edb992ee6266a5840e9799e5/0001-building-utils-don-t-check-verinfo-and-manifests-on-.patch \
+wget https://gist.githubusercontent.com/ookiineko/9099b4c13bf56ddf0724fceea0c0cc91/raw/0c843c58632034e2edb992ee6266a5840e9799e5/0001-building-utils-don-t-check-verinfo-and-manifests-on-.patch \
      -O cygwin.patch
 dos2unix PyInstaller/building/utils.py
 patch -p1 < cygwin.patch

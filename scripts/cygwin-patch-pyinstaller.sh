d43f2b15b4522fbca30d364f58c702cd1a00998f
zjadzhd <116424564+zjadzhd@users.noreply.github.com>
2023-03-14 16:26:03 +0800

    Update cygwin-patch-pyinstaller.sh

--- scripts/cygwin-patch-pyinstaller.sh
+++ scripts/cygwin-patch-pyinstaller.sh
@@ -6,7 +6,5 @@ cd /usr/local/lib/"${python_ver}"/site-packages || exit
 wget https://gist.githubusercontent.com/ookiineko/9099b4c13bf56ddf0724fceea0c0cc91/raw/0c843c58632034e2edb992ee6266a5840e9799e5/0001-building-utils-don-t-check-verinfo-and-manifests-on-.patch \
      -O cygwin.patch
 dos2unix PyInstaller/building/utils.py
-patch -p1 < cygwin.patch
-y
-y
+#patch -p1 < cygwin.patch
 rm -f cygwin.patch
